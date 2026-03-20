9b32bb965e3568a8651ad61779e5cb11f14b815c
Nyakallo <nyamasjhb025@student.wethinkcode.co.za>
2026-03-20 06:28:25 +0200

    Clan class and Jutsu

---
+++ src/main/java/com/nyaks/naruto/sim/Clan/Clan.java
@@ -0,0 +1,10 @@
+package main.java.com.nyaks.naruto.sim.Clan;
+
+public class Clan {
+  private String name;
+  private String village;
+  private String affinity;
+  private String kekkeiGenkai;
+  private String clanJutsu;
+
+}
--- src/main/java/com/nyaks/naruto/sim/Jutsu/Jutsu.java
+++ src/main/java/com/nyaks/naruto/sim/Jutsu/Jutsu.java
@@ -1,10 +1,21 @@
 package main.java.com.nyaks.naruto.sim.Jutsu;
 
-public class Jutsu {
+public abstract class Jutsu {
     private String name;
     private String type;
     private String affinity;
     private int chakraCost;
     private float damage;
 
+    enum TypeOfJutsu {
+        Ninjutsu,
+        Taijutsu,
+        Genjutsu
+    }
+    enum chakraAffinity {
+        Fire,
+        Wind,
+        Water,
+        Lightning
+    }
 }
